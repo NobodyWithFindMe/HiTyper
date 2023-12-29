049805899a81f804690c1a2b27c8b1c500424ad2
NobodyWithFindMe (Ddejj) <154600524+NobodyWithFindMe@users.noreply.github.com>
2023-12-29 14:18:00 +0500

    Добавлены файлы
    1.0

---
+++ HiTyper/Актуальный пароль к программе.bat
@@ -0,0 +1,6 @@
+@echo off 
+color 0a
+title Password in Privet.txt
+:top
+echo %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random% 
+goto top
